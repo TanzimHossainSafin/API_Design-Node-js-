3da7c9b1784f7cf5ce6027c9a7be3d4815d1e8ca
tanzim_safin <tanzimhossainsafin756@gmail.com>
2025-06-05 20:33:14 +0600

    added test for delete review

--- backend/src/handlers/__test__/__review__/deleteReview.test.ts
+++ backend/src/handlers/__test__/__review__/deleteReview.test.ts
@@ -2,7 +2,7 @@ import {describe,it,expect} from "vitest";
 import axios from 'axios';
 describe('/review/customer',()=>{
     it("it will delete a review of a customer about a specific restaurant",async()=>{
-        const result=await axios.delete("http://localhost:3000/app/v1/review/deletereview/cbaea5e8-793d-425f-8f6e-acce16f9cb5e",
+        const result=await axios.delete("http://localhost:3000/app/v1/review/deletereview/935cb76c-14ea-4acc-8d03-f4818e0682f5",
         {
          headers: {
                 Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImUxYWRjZTc4LTJmZjUtNGMwYy04YWQzLTAxYzRlZTU0YTUwOSIsInVzZXJuYW1lIjoiZm9ybG9vcCIsImlhdCI6MTc0OTA0MDE2M30.f0SD_0hAJiEfpZe-x88W6Rt348_IOmp2osfSYMOiRxQ"
